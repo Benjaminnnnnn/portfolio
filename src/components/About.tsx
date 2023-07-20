14abad35958c3e0185a9df770727e1f8ebfa3403
Benjamin Zhuang <zhuanz3@uw.edu>
2023-07-19 21:41:56 -0700

    solved some ts build errors

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { motion } from "framer-motion";
 import Tilt from "react-tilt";
 import { services } from "../constants";
@@ -15,7 +13,7 @@ const ServiceCard = ({ index, title, icon }) => {
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
         <div
-          options={{ max: 45, scale: 1, speed: 450 }}
+          // options={{ max: 45, scale: 1, speed: 450 }}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
