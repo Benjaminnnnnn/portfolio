dc1e66b4d3952e874cce4898e70300f5a31f720f
Benjamin Zhuang <zhuanz3@uw.edu>
2023-07-19 19:42:38 -0700

    fix types

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -4,3 +4,13 @@ declare module "*.jpeg";
 declare module "*.jpg";
 
 declare module "react-tilt";
+declare module "About";
+declare module "Contact";
+declare module "Experience";
+declare module "Feedbacks";
+declare module "Hero";
+declare module "Navbar";
+declare module "Tech";
+declare module "TypingEffect";
+declare module "Works";
+declare module "Canvas";
