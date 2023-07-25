5b8e89916a2547b725e5c21d5c52a66729227eca
Benjamin Zhuang <zhuanz3@uw.edu>
2023-07-24 20:42:29 -0700

    fixed mobile navbar animation bug

--- src/types.d.ts
+++ src/types.d.ts
@@ -35,15 +35,3 @@ export interface IMobileNavbar {
   setToggle: (value: boolean | ((prevToggle: boolean) => boolean)) => void;
   setActive: (value: string | ((prevActive: string) => string)) => void;
 }
-
-declare module "*.png";
-declare module "*.svg";
-declare module "*.jpeg";
-declare module "*.jpg";
-
-declare module "react-tilt";
-declare module "maath/random/dist/maath-random.esm";
-declare module "react-vertical-timeline-component";
-
-declare module "Loader";
-declare module "SectionWrapper";
